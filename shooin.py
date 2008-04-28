c66892f41be2b717bc3ba773eb1c0727a26a2f06
btbytes <pradeep@btbytes.com>
2008-04-28 15:24:41 -0400

    entry.url

--- shooin.py
+++ shooin.py
@@ -13,16 +13,59 @@ from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
-from google.appengine.api.urlfetch import fetch
+from google.appengine.api import urlfetch
 import os
 from lib import utils, markdown2, BeautifulSoup
 from requesthandler import TehRequestHandler
 from blog import Entry
 from lib import demjson
+from StringIO import StringIO
 
+def make_entry(rec):
+    """docstring for make_entry"""
+    body = rec.get('body')
+    body_html = markdown2.markdown(body)
+    rec.update({'body_html': body_html})
+    slug = rec.get('slug')
+    title = rec.get('title')
+    excerpt = rec.get('excerpt')
+    markdown = rec.get('markdown') or 'markdown'
+    tags = rec.get('tags') or []
+    if len(tags) == 0:
+        tags = ['general']
+    tags = [db.Category(utils.slugify(tag)) for tag in tags if tag]
+    
+    static = rec.get('static')
+    
+    if not slug:
+        utils.slugify(title)
+        
+    if not excerpt:
+        soup = BeautifulSoup.BeautifulSoup(body_html)
+        paras = soup.findAll('p')
+        if paras:
+            excerpt = paras[0].string
+    return Entry(author=users.get_current_user(),
+    title=title,
+    slug=slug,
+    body=body,
+    body_html=body_html,
+    markdown=markdown,
+    excerpt=excerpt,
+    tags= tags,
+    static=static,
+    )
+    
 class ShooinHandler(TehRequestHandler):
-    def get(self,fname):
-
-        data = demjson.encode(e, compactly=False)
-        self.render('templates/shooin.html', fname=fname, data=data)
+    def get(self,fname):        
+        #url = 'http://localhost:8080/static/data/%s.json' % fname
+        url = 'http://btbytes.com/default.json'
+        response = urlfetch.fetch(url).content
+        json = demjson.decode(response)
+        titles = [r['title'] for r in json]
+        for r in json:
+            entry = make_entry(r)
+            entry.put()
+        
+        self.render('templates/shooin.html', fname=fname, titles=titles)
         
--- webapp.py
+++ webapp.py
@@ -14,79 +14,10 @@ from google.appengine.ext.webapp import template
 from google.appengine.api import users
 import functools
 import os
-import utils
-
-class Entry(db.Model):
-    author = db.UserProperty()
-    title = db.StringProperty(required=True)
-    slug = db.StringProperty(required=True)
-    body = db.TextProperty(required=True)
-    published = db.DateTimeProperty(auto_now_add=True)
-    updated = db.DateTimeProperty(auto_now=True)
-
-class TehRequestHandler(webapp.RequestHandler):
-    
-    def __init__(self,**kw):
-        webapp.RequestHandler.__init__(TehRequestHandler, **kw)
-        
-    def render(self, tmpl, **kw):
-        template_values = dict(**kw)
-        path = os.path.join(os.path.dirname(__file__), tmpl)
-        self.response.out.write(template.render(path, template_values))
-        
-class MainPageHandler(TehRequestHandler):
-    def get(self):
-        entries = db.Query(Entry).order('-published').fetch(limit=5)
-        self.render("templates/main.html", entries=entries, users=users)
-
-class EntryIndexHandler(TehRequestHandler):
-    def get(self):
-        entries = db.Query(Entry).order('-published').fetch(limit=5)
-        self.render("templates/entryindex.html", entries=entries, users=users)
-
-class EntryHandler(TehRequestHandler):
-    def get(self, slug):
-        entry = db.Query(Entry).filter("slug =", slug).get()
-        if not entry:
-            raise webapp.Error(404)
-        self.render("templates/entry.html", entry=entry)
-
-class FeedHandler(TehRequestHandler):
-    def get(self):
-        entries = db.Query(Entry).order('-published').fetch(limit=10)
-        self.set_header("Content-Type", "application/atom+xml")
-        self.render("templates/atom.xml", entries=entries)
-
-def administrator(method):
-    @functools.wraps(method)
-    def wrapper(self, *args, **kwargs):
-        user = users.get_current_user()
-        if not user:
-            if self.request.method == "GET":
-                self.redirect(users.create_login_url(self.request.uri))
-                return
-        if not users.is_current_user_admin():
-            raise webapp.Error(403)
-        else:
-            return method(self, *args, **kwargs)
-    return wrapper
-
-class NewEntryHandler(TehRequestHandler):
-    @administrator
-    def get(self):
-        self.render("templates/new.html")
-
-    @administrator
-    def post(self):
-        title = self.request.get("title")
-        entry = Entry(
-            author=users.get_current_user(),
-            title=title,
-            slug=utils.slugify(title),
-            body=self.request.get("body"),
-        )
-        entry.put()
-        self.redirect("/entry/" + entry.slug)
+from lib import utils, markdown2, BeautifulSoup
+from requesthandler import TehRequestHandler
+import shooin
+import blog
 
 class LoginHandler(TehRequestHandler):
     def get(self):
@@ -104,15 +35,30 @@ class LogoutHandler(TehRequestHandler):
         else:
             self.redirect('/')
 
+class HomePageHandler(TehRequestHandler):
+    def get(self):
+        entries = blog.Entry.all()
+        entries.filter("static =", False)
+        entries.order('-published').fetch(limit=5)
+        self.render("templates/home.html", entries=entries)
+
+
 def main():
     application = webapp.WSGIApplication([
-        (r"/", MainPageHandler),
-        (r"/entries", EntryIndexHandler),
-        (r"/feed", FeedHandler),
-        (r"/entry/([^/]+)", EntryHandler),
-        (r"/new", NewEntryHandler),
+        (r"/", HomePageHandler),
         (r"/login", LoginHandler),
         (r"/logout", LogoutHandler),
+        
+        (r"/entries", blog.EntryIndexHandler),
+        (r"/feed", blog.FeedHandler),
+        (r"/entry/([^/]+)", blog.EntryHandler),
+        (r"/entry/([^/]+)/edit", blog.NewEntryHandler),
+        (r"/entry/([^/]+)/del", blog.EntryDeleteHandler),
+        (r"/new", blog.NewEntryHandler),
+        (r"/topic/([^/]+)", blog.TagHandler),
+        (r"/shooin/([^/]+)", shooin.ShooinHandler),
+        
+        (r"/([^/]+)", blog.PageHandler),
         ], debug=True)
     wsgiref.handlers.CGIHandler().run(application)
 
